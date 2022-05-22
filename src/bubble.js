4089644b256dd99da3d07ba74d11456574960292
Striborax <88035911+Striborax@users.noreply.github.com>
2022-05-22 18:27:24 +0200

    Add files via upload

---
+++ src/bubble.js
@@ -0,0 +1,132 @@
+function playbubble(){
+    //setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 500;
+
+    let score = 0;
+    let gameFrame = 0;
+    let colour = 'black';
+    let lastRed = 0;
+    let lastYellow = 0;
+    ctx.font = "50px Georgia";
+
+    //mouse
+    let canvasPosition = canvas.getBoundingClientRect();
+    const mouse = {
+        x: canvas.width/2,
+        y: canvas.height/2
+    }
+    window.addEventListener('mousemove', function(event){
+		mouse.x = event.x - canvasPosition.left;
+        mouse.y = event.y - canvasPosition.top;
+	})
+
+    //player
+    class Player{
+        constructor(){
+            this.x = canvas.width/2;
+            this.y = canvas.height/2;
+            this.radius = 50;
+        }
+        update(){
+            if(mouse.x > canvas.width){mouse.x = canvas.width;}
+            if(mouse.x < 0){mouse.x = 0;}
+            if(mouse.y > canvas.height){mouse.y = canvas.height;}
+            if(mouse.y < 0){mouse.y = 0;}
+            const dx = this.x - mouse.x;
+            const dy = this.y - mouse.y;
+            if(mouse.x != this.x){
+                this.x -= dx/20;
+            }
+            if(mouse.y != this.y){
+                this.y -= dy/20;
+            }
+        }
+        draw(){
+            ctx.lineWidth = 0.5;
+            ctx.beginPath();
+            ctx.moveTo(this.x, this.y);
+            ctx.lineTo(mouse.x, mouse.y);
+            ctx.stroke();
+            if(gameFrame - lastYellow > 20){
+                colour = 'black';
+            }
+            ctx.fillStyle = colour;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+    const player = new Player();
+
+    //bubbles
+    const bubblesArray = [];
+    class Bubble{
+        constructor(){
+            this.x = Math.random() * canvas.width;
+            this.y = 100 + canvas.height;
+            this.radius = 50;
+            this.speed = Math.random() * 5 + gameFrame/1000;
+            this.distance;
+            this.counted = false;
+        }
+        update(){
+            this.y -= this.speed;
+            const dx = this.x - player.x;
+            const dy = this.y - player.y;
+            this.distance = Math.sqrt(dx*dx + dy*dy);
+        }
+        draw(color){
+            ctx.fillStyle = color;
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+            ctx.fill();
+            ctx.closePath();
+        }
+    }
+    function handleBubbles(){
+        if(gameFrame % 50 == 0){
+            bubblesArray.push(new Bubble());
+        }
+        for(let i = 0; i < bubblesArray.length; i++){
+            bubblesArray[i].update();
+            bubblesArray[i].draw('#55f');
+        }
+        for(let i = 0; i < bubblesArray.length; i++){
+            if(bubblesArray[i].y < 0 - bubblesArray[i].radius*2){
+                score -= 10;
+                lastRed = gameFrame;
+                bubblesArray.splice(i, 1);
+            }
+            if(bubblesArray[i].distance < bubblesArray[i].radius + player.radius){
+                if(!bubblesArray[i].counted){
+                    score++;
+                    colour = '#0aa';
+                    lastYellow = gameFrame;
+                    bubblesArray[i].counted = true;
+                    bubblesArray.splice(i, 1);
+                }
+            }
+        }
+    }
+
+    //animation
+    function animate(){
+        if(gameFrame - lastRed > 30){
+            ctx.fillStyle = '#ff0';}
+        else{
+            ctx.fillStyle = '#fa0';}
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        handleBubbles();
+        player.update();
+        player.draw();
+        ctx.fillStyle = 'blue';
+        ctx.fillText('score: ' + score, 10, 50);
+        gameFrame++;
+        requestAnimationFrame(animate);
+    }
+    animate();
+}
---
+++ src/shooter.js
@@ -0,0 +1,229 @@
+function playshooter(){
+	//setup
+	const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 800;
+    canvas.height = 500;
+
+	let score = 0;
+	let gameFrame = 0;
+	let bulletFrameLimit = 2;
+	let lastBulletFierd = 0;
+	ctx.font = "50px Georgia";
+
+
+	//mouse
+	let canvasPosition = canvas.getBoundingClientRect();
+    const mouse = {
+        x: canvas.width/2,
+        y: canvas.height/2,
+        click: false
+    }
+	window.addEventListener('mousedown', function(event){
+        mouse.click = true;
+        mouse.x = event.x - canvasPosition.left;
+        mouse.y = event.y - canvasPosition.top;
+    });
+    window.addEventListener('mouseup', function(){
+        mouse.click = false;
+    });
+	window.addEventListener('mousemove', function(event){
+		mouse.x = event.x - canvasPosition.left;
+        mouse.y = event.y - canvasPosition.top;
+	})
+
+	//keyboard
+	const keyboard = {
+		up: false,
+		down: false,
+		left: false,
+		right: false
+	}
+	window.addEventListener('keydown', function(event){
+		switch(event.keyCode){
+			case 87:
+				keyboard.up = true;
+				break;
+			case 65:
+				keyboard.left = true;
+				break;
+			case 83:
+				keyboard.down = true;
+				break;
+			case 68:
+				keyboard.right = true;
+			default:
+				break;
+		}
+	});
+	window.addEventListener('keyup', function(event){
+		switch(event.keyCode){
+			case 87:
+				keyboard.up = false;
+				break;
+			case 65:
+				keyboard.left = false;
+				break;
+			case 83:
+				keyboard.down = false;
+				break;
+			case 68:
+				keyboard.right = false;
+			default:
+				break;
+		}
+	});
+
+
+	//player
+	class Player{
+		constructor(){
+			this.x = canvas.width/2;
+			this.y = canvas.height/2;
+			this.vx = 0;
+			this.vy = 0;
+			this.radius = 25;
+		}
+		update(){
+			if(keyboard.up && this.vy > -1){this.vy-=0.1;}
+			if(keyboard.down && this.vy < 1){this.vy+=0.1;}
+			if(keyboard.left && this.vx > -1){this.vx-=0.1;}
+			if(keyboard.right && this.vx < 1){this.vx+=0.1;}
+			if(this.vx < 0.1 && this.vx > -0.1){this.vx = 0};
+			if(this.vy < 0.1 && this.vy > -0.1){this.vy = 0};
+			if(this.vx < 0){this.vx+=0.02;}
+			if(this.vx > 0){this.vx-=0.02;}
+			if(this.vy < 0){this.vy+=0.02;}
+			if(this.vy > 0){this.vy-=0.02;}
+			this.y+=6*this.vy;
+			this.x+=6*this.vx;
+		}
+		draw(){
+			if(mouse.click){
+                ctx.lineWidth = 0.5;
+                ctx.beginPath();
+                ctx.moveTo(this.x, this.y);
+                ctx.lineTo(mouse.x, mouse.y);
+                ctx.stroke();
+            }
+			ctx.fillStyle = "blue";
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+            ctx.fill();
+            ctx.closePath();
+		}
+	}
+	const player = new Player;
+
+
+	//bullet
+	const bulletArray = [];
+	class Bullet{
+		constructor(){
+			this.x = player.x;
+			this.y = player.y;
+			this.dx = this.x - mouse.x;
+			this.dy = this.y - mouse.y;
+			this.radius = 5;
+		}
+		update(){
+			this.x -= this.dx/Math.sqrt(this.dx*this.dx + this.dy*this.dy)*10;
+			this.y -= this.dy/Math.sqrt(this.dx*this.dx + this.dy*this.dy)*10;
+		}
+		draw(){
+			ctx.fillStyle = "black";
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+            ctx.fill();
+            ctx.closePath();
+		}
+	}
+	function handleBullets(){
+		if(mouse.click && gameFrame - lastBulletFierd > bulletFrameLimit){
+			bulletArray.push(new Bullet);
+			lastBulletFierd = gameFrame;
+		}
+		for(let i = 0; i < bulletArray.length; i++){
+			bulletArray[i].update();
+			bulletArray[i].draw();
+			if(bulletArray[i].x > canvas.width || bulletArray[i].x < 0 || bulletArray[i].y > canvas.height || bulletArray[i].y < 0){
+				bulletArray.splice(i, 1);
+			}
+		}
+	}
+
+
+	//enemy
+	let enemyArray = [];
+	class Enemy{
+		constructor(){
+			if(Math.random() > 0.5){
+				this.direction = -1;
+				this.x = canvas.width;
+			}else{
+				this.direction = 1;
+				this.x = 0;
+			}
+			this.y = Math.random() * canvas.height;
+			this.radius = Math.random() * 50 + 10;
+			this.dx = 0;
+			this.dy = 0;
+			this.distance = 0;
+			this.visible = true;
+		}
+		update(){
+			for(let i = 0; i < bulletArray.length; i++){
+				this.dx = bulletArray[i].x - this.x;
+				this.dy = bulletArray[i].y - this.y;
+				this.distance = Math.sqrt(this.dx*this.dx + this.dy*this.dy);
+				if(this.distance < this.radius && this.visible){
+					this.radius-=5;
+					bulletArray.splice(i, 1);
+				}
+			}
+			this.x += 5*this.direction;
+		}
+		draw(){
+			if(this.visible){
+				ctx.fillStyle = "red";
+			}else{
+				ctx.fillStyle = "#fff";
+			}
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2);
+            ctx.fill();
+            ctx.closePath();
+		}
+	};
+	function handleEnemies(){
+		if(gameFrame % 100 == 0){
+			enemyArray.push(new Enemy());
+		}
+		for(let i = 0; i < enemyArray.length; i++){
+			enemyArray[i].update();
+			enemyArray[i].draw();
+			if(enemyArray[i].radius < 10){
+				enemyArray[i].visible = false;
+			}
+			if(enemyArray[i].direction == 1 && enemyArray[i].x > canvas.width){
+				enemyArray.splice(i, 1);
+			}
+			if(enemyArray[i].direction == -1 && enemyArray[i].x < 0){
+				enemyArray.splice(i, 1);
+			}
+		}
+	}
+
+
+	//update
+	function updateGame(){
+		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		handleEnemies();
+		handleBullets();
+		player.update();
+		player.draw();
+		gameFrame++;
+		requestAnimationFrame(updateGame);
+	}
+	updateGame();
+}
