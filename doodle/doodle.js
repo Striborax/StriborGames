43284e18bd66ab38e209b85b652febe576e47321
Striborax <striborax@gmail.com>
2022-05-26 07:10:29 +0200

    fixed doodle

--- doodle/doodle.js
+++ doodle/doodle.js
@@ -7,8 +7,8 @@ function playdoodle(){
 
 	let score = 0;
 	let gameFrame = 0;
-	let makeNew = false;
 	let distance = 0;
+	let lastNew = 0;
 	ctx.font = "50px Georgia";
 
 
@@ -66,8 +66,10 @@ function playdoodle(){
 			if(this.velocity < 20)
 				this.velocity++;
 			this.y += this.velocity;
-			if(this.y < canvas.height/2)
+			if(this.y < canvas.height/2){
 				this.y = canvas.height/2;
+				distance -= this.velocity;
+			}
 		}
 		draw(){
 			ctx.fillStyle = "red";
@@ -88,8 +90,9 @@ function playdoodle(){
 			this.visible = true;
 		}
 		update(){
-			if(player.y == canvas.height/2)
+			if(player.y == canvas.height/2){
 				this.y-=player.velocity;
+			}
 			if(this.visible && player.y > this.y-this.height && player.y < this.y && player.x-player.width/2 < this.x+this.width/2 && player.x+player.width/2 > this.x-this.width/2 && player.velocity > 0){
 				score++;
 				player.velocity=-25;
@@ -111,12 +114,15 @@ function playdoodle(){
 		thingArray[i+7].y = i*100;
 	}
 	function handleThings(){
+		if(distance - lastNew > 100){
+			thingArray.push(new Thing);
+			lastNew += 100;
+		}
 		for(let i = 0; i < thingArray.length; i++){
 			thingArray[i].update();
 			thingArray[i].draw();
 			if(thingArray[i].visible == false){
 				thingArray.splice(i, 1);
-				thingArray.push(new Thing);
 			}
 		}
 	}
@@ -125,11 +131,11 @@ function playdoodle(){
 	//update
 	function updateGame(){
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
+		handleThings();
 		player.update();
 		player.draw();
-		handleThings();
 		ctx.fillStyle = 'black';
-        ctx.fillText('score: ' + score, 10, 50);
+        ctx.fillText('score: ' + distance, 10, 50);
 		gameFrame++;
 		requestAnimationFrame(updateGame);
 	}
