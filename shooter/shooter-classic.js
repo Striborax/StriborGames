f5d282ac6f3f8197541973836b8a4d0a219f71d6
Striborax <striborax@gmail.com>
2022-05-27 06:58:41 +0200

    fullscreen for shooter

--- shooter/shooter-classic.js
+++ shooter/shooter-classic.js
@@ -2,8 +2,8 @@ function playshooterclassic(){
 	//setup
 	const canvas = document.getElementById('canvas1');
     const ctx = canvas.getContext('2d');
-    canvas.width = 800;
-    canvas.height = 500;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
 	let score = 0;
 	let gameFrame = 0;
